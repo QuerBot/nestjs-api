67235c5dc171f523c716b16c65acbc9fb6d7b623
dstN <github@yinside.de>
2022-04-17 12:53:40 +0200

    Added User

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,4 +1,72 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Dependencies,
+  Get,
+  Post,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+@Dependencies(UserService)
+export class UserController {
+  constructor(userService) {
+    this.userService = userService;
+  }
+
+  @Get()
+  getUser() {
+    return this.userService.getUser();
+  }
+
+  @Get(':id')
+  getUserById() {
+    return this.userService.getUserbyId();
+  }
+
+  @Get(':id/followings')
+  getUserFollowings() {
+    return this.userService.getUserFollowings();
+  }
+
+  @Get(':id/followers')
+  getUserFollowers() {
+    return this.userService.getUserFollowers();
+  }
+
+  @Get(':id/handle')
+  getUserHandle() {
+    return this.userService.getUserHandle();
+  }
+
+  @Post()
+  postUser() {
+    return this.userService.postUser();
+  }
+
+  @Patch(':id')
+  updateUser() {
+    return this.userService.updateUser();
+  }
+
+  @Patch(':id/followings')
+  updateUserFollowings() {
+    return this.userService.updateUserFollowings();
+  }
+
+  @Patch(':id/followers')
+  updateUserFollowers() {
+    return this.userService.updateUserFollowers();
+  }
+
+  @Patch(':id/handle')
+  updateUserHandle() {
+    return this.userService.updateUserHandle();
+  }
+
+  @Delete()
+  deleteUser() {
+    return this.userService.deleteUser();
+  }
+}
--- src/user/user.service.js
+++ src/user/user.service.js
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  getUser() {
+    return 'TODO: getUser';
+  }
+
+  getUserById() {
+    return 'TODO: getUserById';
+  }
+
+  getUserFollowings() {
+    return 'TODO: getUserFollowings';
+  }
+
+  getUserFollowers() {
+    return 'TODO: getUserFollowers';
+  }
+
+  getUserHandle() {
+    return 'TODO: getUserHandle';
+  }
+
+  postUser() {
+    return 'TODO: postUser';
+  }
+
+  updateUser() {
+    return 'TODO: updateUser';
+  }
+
+  updateUserFollowings() {
+    return 'TODO: updateUserFollowings';
+  }
+
+  updateUserFollowers() {
+    return 'TODO: updateUserFollowers';
+  }
+
+  updateUserHandle() {
+    return 'TODO: updateUserHandle';
+  }
+
+  deleteUser() {
+    return 'TODO: deleteUser';
+  }
+}
+
