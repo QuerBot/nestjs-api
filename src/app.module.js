cfedbdd5a03698ca6fe720fbc2a705c838782532
dstN <github@yinside.de>
2022-05-16 14:11:23 +0200

    Tidied up, removed unused stuff

--- src/app.module.js
+++ src/app.module.js
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BubbleModule } from './bubble/bubble.module';
 import { UserModule } from './user/user.module';
 import { TweetModule } from './tweet/tweet.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), BubbleModule, UserModule, TweetModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
