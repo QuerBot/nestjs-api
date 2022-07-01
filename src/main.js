8660a5d0fecfc062aad29b179509d85d57f4bf17
dstN <github@yinside.de>
2022-07-01 18:11:00 +0200

    Changed Passenger

--- src/main.js
+++ src/main.js
@@ -9,10 +9,13 @@ async function bootstrap() {
   app.setGlobalPrefix('v1');
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
+  let appPort;
 
   if (typeof PhusionPassenger !== 'undefined') {
+    appPort = 'passenger';
     app.listen('passenger');
   } else {
+    appPort = 3000;
     app.listen(3000);
   }
   logger.log(`App listening on port ${appPort}`);
